a95db631b0c7a4a047cfc52a11ffb8ccf15fcd40
KOTOKROKETAS <panagiotispeslis@gmail.com>
2025-05-27 18:20:07 +0300

    Delete ΑΑΑ/run.sh

--- ΑΑΑ/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d ".venv" ]; then
-  source .venv/bin/activate
-fi
-
-pip install -r requirements.txt
-
-streamlit run streamlit_pipeline_app.py --server.port=8501 --server.enableCORS=false
